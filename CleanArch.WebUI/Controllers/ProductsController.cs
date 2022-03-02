d62cda0a88aee29b3891ca1e97905c80d749b009
Iamherica <hericacadoni@gmail.com>
2022-03-01 22:59:43 -0300

    add: iniciando a implementação dos métodos actions p o controller products

--- CleanArch.WebUI/Controllers/ProductsController.cs
+++ CleanArch.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using CleanArch.Application.DTOs;
+using CleanArch.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Threading.Tasks;
 
 namespace CleanArch.WebUI.Controllers
@@ -7,9 +9,12 @@ namespace CleanArch.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
-        public ProductsController(IProductService productService)
+        private readonly ICategoryService _categoryService;
+
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -18,5 +23,57 @@ namespace CleanArch.WebUI.Controllers
             var products = await _productService.GetProducts();
             return View(products);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            //View Bag
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+
+        //Receber os dados do formulario
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
+        }
+
+        //Procurar o produto para ver se é válido
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null) return NotFound();
+            var productDto = await _productService.GetById(id);
+
+            if (productDto == null) return NotFound();
+
+            var categories = await _categoryService.GetCategories();
+
+            ViewBag.CategoryId = new SelectList(categories, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+
+        }
+
+        //Postar os dados, validar e retornar os dados já na tabela
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Update(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDto);
+        }
+
+       
     }
 }
